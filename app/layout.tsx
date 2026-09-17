d0ebb4016a45441464c4f513a42709a866d9e3a4
gabribbo3 <gaf90@hotmail.it>
2026-09-17 14:56:38 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Script from 'next/script';
 
 import PushBridge from './PushBridge';
 import VisitTracker from './VisitTracker';
+import CookieConsent from './CookieConsent';
 
 export const metadata: Metadata = {
   title: 'LavoroSubito',
@@ -24,6 +25,35 @@ export default function RootLayout({
 
         <PushBridge />
         <VisitTracker />
+        <CookieConsent />
+
+        {/*
+          Consent Mode viene inizializzato PRIMA
+          del caricamento del tag Google Ads.
+
+          Finché l'utente non sceglie,
+          i consensi opzionali restano negati.
+        */}
+        <Script
+          id="google-consent-default"
+          strategy="beforeInteractive"
+        >
+          {`
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag(){
+              dataLayer.push(arguments);
+            }
+
+            gtag('consent', 'default', {
+              ad_storage: 'denied',
+              analytics_storage: 'denied',
+              ad_user_data: 'denied',
+              ad_personalization: 'denied',
+              wait_for_update: 500
+            });
+          `}
+        </Script>
 
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-18451155996"
@@ -36,9 +66,19 @@ export default function RootLayout({
         >
           {`
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-18451155996');
+
+            window.gtag =
+              window.gtag ||
+              function(){
+                window.dataLayer.push(arguments);
+              };
+
+            window.gtag('js', new Date());
+
+            window.gtag(
+              'config',
+              'AW-18451155996'
+            );
           `}
         </Script>
       </body>
